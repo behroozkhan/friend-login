682224bfa653c3cc32a9461dde7692e692abe797
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-08-18 03:59:17 +0500

    working on

---
+++ login.js
@@ -0,0 +1,21 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+ import {getAuth}  from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+   apiKey: "AIzaSyDKckeGedMbGSj7NpxFk3siDAXkzd-yGU0",
+   authDomain: "friend-hackthon.firebaseapp.com",
+   projectId: "friend-hackthon",
+   storageBucket: "friend-hackthon.appspot.com",
+   messagingSenderId: "229955689782",
+   appId: "1:229955689782:web:29c41164559f544a626797",
+   measurementId: "G-ZST5B4N4VQ"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ const auth = getAuth(app);
---
+++ register.js
@@ -0,0 +1,132 @@
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  db,
+  doc,
+  onAuthStateChanged,setDoc
+} from "./firbaseConfig.js";
+
+
+
+onAuthStateChanged(auth,(user) => {
+    if (user) {
+        const uid = user.uid;
+        console.log("uid===>",uid)
+        window.location.href = "./profiel.html";
+        // console.log('User uid-->', uid)
+        // loader_container.style.display = 'none'
+        // createAccountContainer.style.display = 'none'
+        // content_container.style.display = 'block'
+        // getPosts()
+        // const info = await getUserInfo(uid)
+        // welcome.innerHTML = `Welcome ${info.name}`
+        // ...
+    } else {
+        console.log("user Sign Out");
+        // console.log('User is not logged in')
+        // loader_container.style.display = 'none'
+        // createAccountContainer.style.display = 'block'
+        // content_container.style.display = 'none'
+
+    }
+});
+
+
+const registerForm = document.querySelectorAll("#register-form")[0];
+// const registerBtn = document.querySelectorAll("#register-btn")[0];
+registerForm.addEventListener('submit',(e) =>{
+    e.preventDefault();
+       // Show the loader
+       loader.classList.remove('d-none');
+       let userName = document.querySelectorAll("#user-name")[0].value;
+    let email = document.querySelectorAll("#email-register")[0].value;
+    let password = document.querySelectorAll("#password-register")[0].value;
+  
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(async (userCredential) => {
+        const user = userCredential.user;
+        console.log("user-register", user);
+        const userInfo = {
+            userName,
+            email,
+            uid: user.uid,
+        };
+        
+        const ecomUserRef = doc(db, "ecom_user", user.uid);
+        console.log("ecomUserRef", ecomUserRef);
+        await setDoc(ecomUserRef, userInfo);
+        
+        // Hide the loader
+        loader.classList.add('d-none');
+        
+        window.location.href = "./profiel.html";
+        console.log("user Sign Up");
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("error", error);
+        // ..
+    });
+})
+
+// const register = (e) => {
+    //   e.preventDefault();
+    //   let userName = document.querySelectorAll("#user-name")[0].value;
+    //   let email = document.querySelectorAll("#email-register")[0].value;
+    //   let password = document.querySelectorAll("#password-register")[0].value;
+    
+    //   createUserWithEmailAndPassword(auth, email, password)
+    //     .then(async (userCredential) => {
+        //       const user = userCredential.user;
+        //       console.log("user-register", user);
+        //       const userInfo = {
+            //         userName,
+            //         email,
+            //         uid: user.uid,
+            //       };
+            
+            //       const ecomUserRef = doc(db, "ecom_user", user.uid);
+            //       console.log("ecomUserRef", ecomUserRef);
+            //       await setDoc(ecomUserRef, userInfo);
+            //       console.log("user Sign Up");
+            //     })
+            //     .catch((error) => {
+                //       const errorCode = error.code;
+                //       const errorMessage = error.message;
+                //       console.log("error", error);
+                //       // ..
+                //     });
+                // };
+                // registerBtn.addEventListener("submit", register);
+                
+                // const registerBtn = document.querySelectorAll("#register-btn")[0];
+                
+                //  const register = () =>{
+                    //     let userName = document.querySelectorAll("#user-name")[0].value;
+                    //     let emailReg = document.querySelectorAll("#email-register")[0].value;
+    //     let passwordReg = document.querySelectorAll("#password-register")[0].value;
+    
+    //     const userObj = {
+        //         emailReg,
+        //         userName;
+        //     }
+        
+        // const auth = getAuth();
+        // createUserWithEmailAndPassword(auth, emailReg, passwordReg)
+        //   .then((userCredential) => {
+            //     // Signed in
+            //     const user = userCredential.user;
+            //     console.log("user-register",user);
+            //     // ...
+            
+            //   })
+            //   .catch((error) => {
+                //     const errorCode = error.code;
+                //     const errorMessage = error.message;
+                //     console.log("error",error);
+                //     // ..
+                //   });
+                //  }
+                
+                //  registerBtn.addEventListener("click",register)
